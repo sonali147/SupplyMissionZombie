395d9def3e898b5d0cf7404b8356560d7cf1e0e8
Sonali Gupta <sonaligupta2510@gmail.com>
2020-11-11 23:47:16 +0530

    changes regards to sprite, convert to body

--- sketch.js
+++ sketch.js
@@ -21,6 +21,9 @@ function preload() {
 	zombieRightImg = loadImage("zombie right.jpg");
 	Heading = loadImage("Text.png");
 	Heading2 = loadImage("Text2.png");
+	//Loading sounds to two variables.
+	zombie_sound = loadSound("zombie_sound.mp3");
+	success_sound = loadSound("win.mp3");
 }
 
 //setup function.
@@ -28,7 +31,7 @@ function setup() {
 	//Creating the canvas area.
 	createCanvas(800, 700);
 	//Setting rectMode as CENTER.
-	rectMode(CENTER);
+	//rectMode(CENTER);
 
 	//Creating a sprites for heading.
 	//Adding image to them.
@@ -40,9 +43,6 @@ function setup() {
 	headText2.addImage(Heading2);
 	headText2.scale = 0.2;
 
-	//Loading sounds to two variables.
-	zombie_sound = loadSound("zombie_sound.mp3");
-	success_sound = loadSound("win.mp3");
 
 	//Creating a sprite named zombieLeft.
 	zombieLeft = createSprite(-50,610,10,10);
@@ -59,7 +59,7 @@ function setup() {
 	zombieRight.scale = 0.2;
 
 	//Creating a sprite named packageSprite.
-	packageSprite=createSprite(50, 50, 10,10);
+	packageSprite=createSprite(50, 80, 10,10);
 	//Adding image to it.
 	packageSprite.addImage(packageIMG);
 	//Adjusting its size.
@@ -73,9 +73,9 @@ function setup() {
 	helicopterSprite.scale = 0.6;
 
 	//Creating a sprite named groundSprite.
-	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite=createSprite(width/2, 670, width,50);
 	//Giving white color to it.
-	groundSprite.shapeColor=color(255);
+	//groundSprite.shapeColor=color(255);
 
 	//Creating an Engine and storing it in the variable engine.
 	engine = Engine.create();
@@ -83,47 +83,50 @@ function setup() {
 	world = engine.world;
 
 	//Creating a circle body named packageBody.
-	packageBody = Bodies.circle(50 , 70 , 5 , {restitution:0, isStatic:true});
+	packageBody = Bodies.rectangle(50 , 80 , 10, 10 , {restitution:0, isStatic:true});
 	//Adding it to Matter.World.
 	World.add(world, packageBody);
 
 	//Creating a rectangle body named ground.
-	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+	ground = Bodies.rectangle(width/2, 670, width, 50 , {isStatic:true} );
 	//Adding it to Matter.World.
 	World.add(world, ground);
 
 	//Creating three sprites for the red box.
 	//Giving red color to them.
-	leftBoxSprite=createSprite(370, 635, 10,50);
+	leftBoxSprite=createSprite(360, 620, 10,50);
 	leftBoxSprite.shapeColor=color(255,0,0);
-	bottomBoxSprite=createSprite(400, 655, 50,10);
+	bottomBoxSprite=createSprite(400, 630, 70,30);
 	bottomBoxSprite.shapeColor=color(255,0,0);
-	rightBoxSprite=createSprite(430, 635, 10,50);
+	rightBoxSprite=createSprite(440, 620, 10,50);
  	rightBoxSprite.shapeColor=color(255,0,0);
 
 	//Creating three rectangle bodies for the red box.
 	//Adding them to Matter.world.
- 	leftBoxBody = Bodies.rectangle(370, 635, 10,70 , {isStatic:true} );
+ 	leftBoxBody = Bodies.rectangle(360, 620, 10,70 , {isStatic:true} );
  	World.add(world, leftBoxBody);
- 	bottomBoxBody = Bodies.rectangle(400, 655, 50,10 , {isStatic:true} );
+ 	bottomBoxBody = Bodies.rectangle(400, 630, 70,30 , {isStatic:true} );
  	World.add(world, bottomBoxBody);
- 	rightBoxBody = Bodies.rectangle(430, 635, 10,70 , {isStatic:true} );
+ 	rightBoxBody = Bodies.rectangle(440, 620, 10,70 , {isStatic:true} );
 	World.add(world, rightBoxBody);
 
 	//Running the previously created engine.
-	Engine.run(engine);
+	
 }
 
 //draw function.
 function draw() {
+	Engine.update(engine);
 	//Setting rectMode as CENTER.
-	rectMode(CENTER);
+	//rectMode(CENTER);
 	//Setting background color as black.
 	background(rgb(46,48,47));
+
+	//rect(ground.position.x, ground.position.y, width, 50);
 	
 	//Setting packageSprite's x and y position same as packageBody's x and y position.
 	packageSprite.x = packageBody.position.x;
-	packageSprite.y = packageBody.position.y;
+	packageSprite.y = packageBody.position.y-10;
 
 	//Setting leftBoxSprite's x and y position same as leftBoxBody's x and y position.
 	leftBoxSprite.x = leftBoxBody.position.x;
@@ -137,7 +140,11 @@ function draw() {
 	bottomBoxSprite.x = bottomBoxBody.position.x;
 	bottomBoxSprite.y = bottomBoxBody.position.y;
 
+	groundSprite.x = ground.position.x;
+	groundSprite.y = ground.position.y;
+
 	//Displaying 'Well Done!' text under certain conditions.
+	/*
 	if(packageSprite.isTouching(bottomBoxSprite) && packageSprite.x > 375 && packageSprite.x < 425) {
 		if(zombieLeft.velocityX === 0 || zombieRight.velocityX === 0) {
 			fill("red");
@@ -149,24 +156,28 @@ function draw() {
 	}
 
 	//Colliding packageSprite with bottomBoxSprite.
-	packageSprite.collide(bottomBoxSprite);
-	packageSprite.collide(leftBoxSprite);
-	packageSprite.collide(rightBoxSprite);
+	//packageSprite.collide(bottomBoxSprite);
+	//packageSprite.collide(leftBoxSprite);
+	//packageSprite.collide(rightBoxSprite);
 
 	//Changing zombieLeft and zombieRight's velocity when packageSprite touches groundSprite.
-	if(packageSprite.isTouching(groundSprite)) {
-		
-		Matter.Body.setStatic(packageBody,true);
-		if(packageSprite.x < 400 && packageSprite.y < 800) {
+	*/
+	//if(packageSprite.isTouching(groundSprite)) {
+	if(packageBody.position.y > 600) {	
+		//Matter.Body.setStatic(packageBody,true);
+		if(packageBody.position.x > 10 && packageBody.position.x < 350) {
 			zombieLeft.velocityX = 3;
 		}
-		else if(packageSprite.x > 400 && packageSprite.y < 800) {
+		else if(packageBody.position.x > 450 && packageBody.position.x < 800) {
 			zombieRight.velocityX = -3;
 		}
 	} 
 
+    
 	//Assigning functions when packageSprite touches zombieLeft, zombieRight or packageSprite's y position is greater than 800.
-	if(packageSprite.isTouching(zombieLeft) || packageSprite.isTouching(zombieRight) || packageSprite.y > 800) {
+	//if(packageSprite.isTouching(zombieLeft) || packageSprite.isTouching(zombieRight) || packageSprite.y > 800) {
+	//console.log(packageBody.position.x - zombieLeft.x);
+	if((packageBody.position.x - zombieLeft.x < 30) || (zombieRight.x - packageBody.position.x < 30)) {
 		//Setting zombieLeft and zombieRight's velocityX to 0.
 		zombieLeft.velocityX = 0;
 		zombieRight.velocityX = 0;
@@ -181,6 +192,7 @@ function draw() {
 		text("Refresh the page to retry.", 210,500);
 	}
 
+	/*
 	//Setting a condition when Right Arrow key is pressed.
 	//If the condition is true, zombie_sound is played; else success_sound is played.
 	if(keyDown(RIGHT_ARROW)) {
@@ -195,7 +207,7 @@ function draw() {
 			}
 		}
 	}
-
+	*/
 	//Displaying info text.
 	fill("white");
 	textFont("segoe script");
@@ -212,25 +224,27 @@ function draw() {
 //keyPressed function.
 function keyPressed() {
 	//Moving helicopterSprite towards left when down arrow key is pressed and packageSprite's y position is less than 200.
-	if (keyCode === DOWN_ARROW && packageSprite.y < 200) {
+	if (keyCode === DOWN_ARROW) {
 		helicopterSprite.x=helicopterSprite.x-30;    
 		if(packageSprite.y < 200) {
-			translation={x:-30,y:0}
-			Matter.Body.translate(packageBody, translation)	
+			//translation={x:-30,y:0}
+			//Matter.Body.translate(packageBody, translation)	
+			Matter.Body.setPosition(packageBody, {x:packageBody.position.x - 30, y :packageBody.position.y})	
 		}
 	} 
 	
 	//Moving helicopterSprite towards right when control key is pressed and packageSprite's y position is less than 200.
-	else if (keyCode === CONTROL && packageSprite.y < 200) {
+	else if (keyCode === CONTROL) {
 		helicopterSprite.x=helicopterSprite.x+30;
 		if(packageSprite.y < 200) {
-			translation={x:30,y:0}
-			Matter.Body.translate(packageBody, translation)	
+			//translation={x:30,y:0}
+			//Matter.Body.translate(packageBody, translation)	
+			Matter.Body.setPosition(packageBody, {x:packageBody.position.x + 30, y :packageBody.position.y})	
 		}
 	}
 
 	//Making the packageBody fall on ground when Right arrow key is pressed and packageSprite's y is less than 200.
- 	else if (keyCode === RIGHT_ARROW && packageSprite.y < 200) {
+ 	else if (keyCode === RIGHT_ARROW ) {
     	Matter.Body.setStatic(packageBody, false);
   	}
 }
